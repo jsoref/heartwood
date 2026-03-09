1b986af0060a10412df0c70705377c17622f5756
Fintan Halpenny <fintan.halpenny@gmail.com>
2026-03-09 16:43:37 +0000

    cli/tests: Refactor workflow test

--- crates/radicle-cli/tests/commands.rs
+++ crates/radicle-cli/tests/commands.rs
@@ -30,6 +30,7 @@ mod commands {
     mod sync;
     mod utility;
     mod watch;
+    mod workflow;
 }
 
 /// Run a CLI test file.
@@ -134,56 +135,6 @@ fn rad_remote() {
         "examples/rad-remote.md",
         environment.work(&alice),
         Some(&home),
-        [],
-    )
-    .unwrap();
-}
-
-#[test]
-fn rad_workflow() {
-    let mut environment = Environment::new();
-    let alice = environment.node("alice");
-    let bob = environment.node("bob");
-
-    environment.repository(&alice);
-
-    environment.test("workflow/1-new-project", &alice).unwrap();
-
-    let alice = alice.spawn();
-    let mut bob = bob.spawn();
-
-    bob.connect(&alice).converge([&alice]);
-
-    environment.test("workflow/2-cloning", &bob).unwrap();
-
-    test(
-        "examples/workflow/3-issues.md",
-        environment.work(&bob).join("heartwood"),
-        Some(&bob.home),
-        [],
-    )
-    .unwrap();
-
-    test(
-        "examples/workflow/4-patching-contributor.md",
-        environment.work(&bob).join("heartwood"),
-        Some(&bob.home),
-        [],
-    )
-    .unwrap();
-
-    test(
-        "examples/workflow/5-patching-maintainer.md",
-        environment.work(&alice),
-        Some(&alice.home),
-        [],
-    )
-    .unwrap();
-
-    test(
-        "examples/workflow/6-pulling-contributor.md",
-        environment.work(&bob).join("heartwood"),
-        Some(&bob.home),
         [],
     )
     .unwrap();
