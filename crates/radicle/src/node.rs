jsoref/heartwood

commit a2e72b48e79d090d33f6c13c485239947de0522e

node: Serialize and deserialize ipv6 addresses in square brackets

crates/radicle/src/node.rs
@@ -427,6 +427,7 @@ impl TryFrom<&sqlite::Value> for Alias {
 #[derive(Clone, Eq, PartialEq, Debug, Hash, From, Wrapper, WrapperMut, Serialize, Deserialize)]
 #[wrapper(Deref)]
 #[wrapper_mut(DerefMut)]
+#[serde(try_from = "String", into = "String")]
 #[cfg_attr(
     feature = "schemars",
     derive(schemars::JsonSchema),
@@ -436,7 +437,6 @@ impl TryFrom<&sqlite::Value> for Alias {
 ")
 )]
 pub struct Address(
-    #[serde(with = "crate::serde_ext::string")]
     #[cfg_attr(feature = "schemars", schemars(
         with = "String",
         regex(pattern = r"^.+:((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$"),
@@ -523,6 +523,20 @@ impl Display for Address {
             }
             _ => self.0.fmt(f),
         }
+    }
+}
+
+impl TryFrom<String> for Address {
+    type Error = <Self as FromStr>::Err;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.parse()
+    }
+}
+
+impl From<Address> for String {
+    fn from(value: Address) -> Self {
+        value.to_string()
     }
 }
 

commit c237a3fe479050691001ad7487c6ec151dbb797b

radicle: Fix JSON Schema for `node::Address`
JSON Schema generation via `schemars` was accidentally broken in
`a2e72b4`. Restore the schema.

crates/radicle/src/node.rs
@@ -433,21 +433,17 @@ impl TryFrom<&sqlite::Value> for Alias {
     derive(schemars::JsonSchema),
     schemars(description = "\
     An IP address, or a DNS name, or a Tor onion name, followed by the symbol ':', \
-    followed by a TCP port number.\
-")
-)]
-pub struct Address(
-    #[cfg_attr(feature = "schemars", schemars(
-        with = "String",
-        regex(pattern = r"^.+:((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$"),
-        extend("examples" = [
+    followed by a TCP port number.",
+    extend(
+        "examples" = [
             "xmrhfasfg5suueegrnc4gsgyi2tyclcy5oz7f5drnrodmdtob6t2ioyd.onion:8776",
             "seed.example.com:8776",
             "192.0.2.0:31337",
-        ]),
-    ))]
-    NetAddr<HostName>,
-);
+        ],
+        "pattern" = "^.+:((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$",
+    ),
+))]
+pub struct Address(NetAddr<HostName>);
 
 impl Address {
     /// Check whether this address is from the local network.